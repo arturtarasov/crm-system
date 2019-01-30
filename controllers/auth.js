88cd6b543106bf550dc0110b3f189659679f77b8
tarasov.a <tarasov.art.ser@gmail.com>
2019-01-30 18:11:39 +0300

    controllers, routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,12 @@
 module.exports.login = function(req, res) {
-    res.status(200).json({
-        login: 'from controller'
-    });
+  res.status(200).json({
+    login: 'from controller'
+  })
 }
 
-module.exports.registr = function(req, res) {
-    res.status(200).json({
-        registr: 'from controller'
-    });
+
+module.exports.register = function(req, res) {
+  res.status(200).json({
+    register: 'from controller'
+  })
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const contrller = require('../controllers/auth');
-const router = express.Router();
+const express = require('express')
+const controller = require('../controllers/auth')
+const router = express.Router()
+
+// localhost:5000/api/auth/login
+router.post('/login', controller.login)
+
+// localhost:5000/api/auth/register
+router.post('/register', controller.register)
 
 
-//localhost:5000/api/auth/login
-router.get('/login', contrller.login);
-
-//localhost:5000/api/auth/registr
-router.get('/registr', contrller.registr);
-
-
-module.exports = router;
+module.exports = router
