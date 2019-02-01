95e78f460b257738996455447428c10e71f728f7
tarasov.a <tarasov.art.ser@gmail.com>
2019-02-01 15:32:07 +0300

    create category

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(passport.initialize());
 require('./middleware/passport')(passport);
 
 app.use(require('morgan')('dev'));
-
+app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
