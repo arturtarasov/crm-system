47ea9aaf9b1ece14e2014d2ff4510a454875f875
Artur <artur-tarasov@mail.ru>
2019-07-30 21:43:11 +0300

    commit to heroku

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require('express'); 
+const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const path = require('path');
 
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
@@ -11,16 +12,16 @@ const positionRoutes = require('./routes/position');
 const keys = require('./config/keys');
 const app = express()
 
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true , useCreateIndex: true})
-    .then(() => {console.log('MongoDB connected')})
-    .catch(error => {console.log(error)});
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useCreateIndex: true })
+    .then(() => { console.log('MongoDB connected') })
+    .catch(error => { console.log(error) });
 
 app.use(passport.initialize());
 require('./middleware/passport')(passport);
 
 app.use(require('morgan')('dev'));
 app.use('/uploads', express.static('uploads'))
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(require('cors')());
@@ -31,5 +32,17 @@ app.use('/api/category', categoryRoutes);
 app.use('/api/order', orderRoutes);
 app.use('/api/position', positionRoutes);
 
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/dist/client'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(
+                __dirname, 'client', 'dist', 'client', 'index.html'
+            )
+        )
+    })
+}
 
 module.exports = app;
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://artur:qwertyuiop1234567890@ds247430.mlab.com:47430/crm-system',
-    jwt: 'dev-jwt'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys.prod')
+} else {
+  module.exports = require('./keys.dev')
 }
